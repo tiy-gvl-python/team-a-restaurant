a7fcba4a0626985c9343acf3ffccda1fd64b98ac
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-09 16:42:42 -0400

    merged in db.sqlite3

--- project_team_a/project_team_a/urls.py
+++ project_team_a/project_team_a/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from restaurant_app.views import home
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
