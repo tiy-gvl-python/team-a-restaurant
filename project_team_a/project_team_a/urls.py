9cac560edae97d3a7d27760362cbd758a2a0e2ee
Bekk Blando <bekkblando@gmail.com>
2015-07-09 14:06:16 -0400

    Added bootstrap

--- project_team_a/project_team_a/urls.py
+++ project_team_a/project_team_a/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from restaurant_app.views import home
 
 urlpatterns = [
+
+    url(r'^', home, name="home"),
     url(r'^admin/', include(admin.site.urls)),
 ]
