98922355a73b057fa52334a630953ef45a110814
Jeff Hacker <reachjkh@yahoo.com>
2015-07-09 21:52:27 -0400

    delete_view, create_view, list_view, urls

--- project_team_a/restaurant_app/urls.py
+++ project_team_a/restaurant_app/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
-from restaurant_app.views import home
+from .views import home, ItemCreateView, ItemDeleteView, ItemListView
+
 
 urlpatterns = [
 
+    url(r'^item_form/', ItemCreateView.as_view(), name="item_form"),
+    url(r'^item_list/', ItemListView.as_view(template="item_list.html"), name="item_list"),
+    url(r'^delete_item/(?P<pk>\d+)$', ItemDeleteView.as_view(), name="delete_item"),
     url(r'^', home, name="home"),
+
 
 
 ]
--- project_team_a/restaurant_app/views.py
+++ project_team_a/restaurant_app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, render_to_response
-
-# Create your views here.
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from .models import Item
 
 def home(requests):
     return render_to_response("home.html")
 
+class ItemListView(ListView):
+    model = Item
+    template = "item_list.html"
+
+
+class ItemCreateView(CreateView):
+    model = Item
+    fields = ['name', 'price']
+    success_url = reverse_lazy('/item_list')
+
+
+class ItemDeleteView(DeleteView):
+    model = Item
+    success_url = '/item_list/'
